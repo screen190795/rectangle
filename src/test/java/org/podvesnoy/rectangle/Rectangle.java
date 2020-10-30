c6105bab3acb11eea4742083d5c9142762a1e5af
= <horizonjournal78@gmail.com>
2020-10-30 16:16:36 +0300

    first commit

---
+++ src/test/java/org/podvesnoy/rectangle/Rectangle.java
@@ -0,0 +1,17 @@
+package org.podvesnoy.rectangle;
+
+public class Rectangle {
+
+    public int length;
+    public int width;
+    public Rectangle (int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+    public int getArea() {
+        return length * width;
+    }
+    public int getPerimeter() {
+        return (length + width) * 2;
+    }
+}
